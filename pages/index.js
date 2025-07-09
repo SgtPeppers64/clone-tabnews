265b68287a9c46224300360f044886c92b8457ea
SgtPeppers64 <104874348+SgtPeppers64@users.noreply.github.com>
2025-07-09 01:06:40 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
 function Home() {
-    return <h1>Bem-vindo ao site!</h1>
+  return <h1>Bem-vindo ao site!</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 export default Home;
