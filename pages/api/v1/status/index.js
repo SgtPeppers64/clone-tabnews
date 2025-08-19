fd29ca5865ff55232c51f77a03fb44a5970be48b
SgtPeppers64 <104874348+SgtPeppers64@users.noreply.github.com>
2025-08-19 14:09:17 +0000

    finaliza o endpoint de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,33 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updateAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SELECT version();");
+  const databaseVersionValue = databaseVersionResult.rows[0].version;
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue = await response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      version: databaseVersionValue,
+      max_connections: parseInt(databaseMaxConnectionsValue),
+      opened_connections: databaseOpenedConnectionsValue,
+    },
+  });
 
-  response.status(200).json({ "chave ": "são acima da media" });
+  const databaseName = process.env.POSTGRES_DB;
+  console.log("Banco de dados selecionado: ${databaseName}");
+
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = '" +
+      databaseName +
+      "';",
+    //"SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db´
+  );
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 }
 
 export default status;
