02735c09ba6d50d9fa2b6cc838a01feac2008597
SgtPeppers64 <104874348+SgtPeppers64@users.noreply.github.com>
2025-08-25 19:36:07 +0000

    corriginto digitação

--- infra/database.js
+++ infra/database.js
@@ -12,7 +12,7 @@ async function query(queryObject) {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRESS_DB,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
 
